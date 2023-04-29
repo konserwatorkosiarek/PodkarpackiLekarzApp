92be221d6ca14b1bc173ace541be299c4aa3c2f0
Kamil Witas <kamilwitas1@gmail.com>
2023-04-29 09:06:00 +0200

    Added shared class library, Roles, Permissions

---
+++ src/PodkarpackiLekarz.Shared/Identity/Permissions.cs
@@ -0,0 +1,30 @@
+﻿namespace PodkarpackiLekarz.Shared.Identity;
+
+public static class Permissions
+{
+    public static string[] AllPermissions => GetAllPermissions();
+
+    public const string ManageUsers = "manage-users";
+
+    public static string[] GetPermissions(Role role)
+    {
+        return role switch
+        {
+            Role.Admin => AdminPermissions,
+            Role.Patient => PatientPermissions,
+            Role.Doctor => DoctorPermissions,
+            _ => PatientPermissions
+        };
+    }    
+
+    private static string[] AdminPermissions = new string[] { ManageUsers };
+    private static string[] PatientPermissions = new string[] { };
+    private static string[] DoctorPermissions = new string[] { };
+
+    private static string[] GetAllPermissions()
+    {
+        var permissions = AdminPermissions.Concat(PatientPermissions).Concat(DoctorPermissions);
+
+        return permissions.Distinct().ToArray();
+    }        
+}
---
+++ src/PodkarpackiLekarz.Shared/Identity/Role.cs
@@ -0,0 +1,9 @@
+﻿namespace PodkarpackiLekarz.Shared.Identity
+{
+    public enum Role
+    {
+        Admin = 0,
+        Patient = 1,
+        Doctor = 2
+    }
+}
